8f08e564f79bcd50149887c5c4b0a4dc130a37d3
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-12-26 12:11:51 +0000

    Use OmegaConf for configuration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+import datetime
 
+from omegaconf import OmegaConf
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,7 +13,7 @@ from networks import SimpleConvNet
 from loggers import get_default_logger
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(CONFIG, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -20,12 +22,10 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % CONFIG.LOG.INTERVAL == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
 
 
 def test(model, device, test_loader):
@@ -47,39 +47,25 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
+def main(CONFIG):
+    # Initialize logger
+    logger = get_default_logger()
 
-    torch.manual_seed(args.seed)
+    # Set device
+    if CONFIG.USE_GPU is None: CONFIG.USE_GPU = torch.cuda.is_available()
+    if CONFIG.USE_GPU: logger.info("Using GPU 💨")
+    else: logger.warn("Using CPU 🐌")
+    device = torch.device("cuda" if CONFIG.USE_GPU else "cpu")
 
-    device = torch.device("cuda" if use_cuda else "cpu")
+    # Set random seed for reproducibility
+    if CONFIG.SEED is None:
+        CONFIG.SEED = datetime.datetime.now().strftime("%Y%m%d%H%M%S%z")
+        logger.info(f"Random seed not specified: using seed {CONFIG.SEED} 🎲")
+    torch.manual_seed(CONFIG.SEED)
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
+    train_kwargs = {'batch_size': CONFIG.TRAIN.BATCH_SIZE}
+    test_kwargs = {'batch_size': CONFIG.TEST.BATCH_SIZE}
+    if CONFIG.USE_GPU:
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
                        'shuffle': True}
@@ -98,17 +84,21 @@ def main():
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = SimpleConvNet().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    optimizer = optim.Adadelta(model.parameters(), lr=CONFIG.TRAIN.LEARNING_RATE)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+    scheduler = StepLR(optimizer, step_size=1, gamma=CONFIG.TRAIN.LEARNING_RATE_GAMMA)
+    for epoch in range(1, CONFIG.TRAIN.EPOCHS + 1):
+        train(CONFIG, model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader)
         scheduler.step()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    torch.save(model.state_dict(), "mnist_cnn.pth")
 
 
 if __name__ == "__main__":
-    main()
+    # Load Configuration
+    YAML_CONFIG = OmegaConf.load("configs/default.yaml")
+    CLI_CONFIG = OmegaConf.from_cli()
+    CONFIG = OmegaConf.merge(YAML_CONFIG, CLI_CONFIG)
+
+    main(CONFIG)
